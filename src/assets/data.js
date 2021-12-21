651e50227e1f25e14311d7333625b24f9236c14a
Alejandro <arodacademic@gmail.com>
2021-12-21 11:26:58 -0500

    refactor: Fix data to fit new images layout

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,14 +1,16 @@
 import im1 from './pictures/2017_AM_Potfolio_DiDa_page-0001.jpg';
 import im2 from './pictures/2017_AM_Potfolio_DiDa_page-0002.jpg';
-import im3 from './pictures/2017_AM_Potfolio_DiDa_page-0003.jpg';
-import im17 from './pictures/2017_AM_Potfolio_DiDa_page-0017.jpg';
+import im3 from './pictures/2017_AM_Potfolio_DiDa_page-0002 (1).jpg';
+import im4 from './pictures/2017_AM_Potfolio_DiDa_page-0003.jpg';
+import im5 from './pictures/2017_AM_Potfolio_DiDa_page-0003 (1).jpg';
+import im6 from './pictures/2017_AM_Potfolio_DiDa_page-0017.jpg';
 
 const data = {
   home: {
     file: im1,
   },
   contact: {
-    file: im17,
+    file: im6,
   },
   pages: {
     1: {
@@ -18,43 +20,24 @@ const data = {
         '"Maestro de Obra" for the extension of the Havana Club rum factory in San José de las Lajas',
       year: 2019,
       index: true,
-      pages: [1],
+      pages: [1, 2],
     },
     2: {
       file: im3,
-      header: 'Women Centers',
-      subheader:
-        'Prototype: for 17 rural communities in Swaziland. Built 1st stage in Mambane and Lwandle',
-      year: 2017,
-      index: true,
-      pages: [2],
+      index: false,
     },
     3: {
-      file: im3,
+      file: im4,
       header: 'Women Centers',
       subheader:
         'Prototype: for 17 rural communities in Swaziland. Built 1st stage in Mambane and Lwandle',
       year: 2017,
       index: true,
-      pages: [2],
+      pages: [3, 4],
     },
     4: {
-      file: im3,
-      header: 'Women Centers',
-      subheader:
-        'Prototype: for 17 rural communities in Swaziland. Built 1st stage in Mambane and Lwandle',
-      year: 2017,
-      index: true,
-      pages: [2],
-    },
-    5: {
-      file: im3,
-      header: 'Women Centers',
-      subheader:
-        'Prototype: for 17 rural communities in Swaziland. Built 1st stage in Mambane and Lwandle',
-      year: 2017,
-      index: true,
-      pages: [2],
+      file: im5,
+      index: false,
     },
   },
 };
