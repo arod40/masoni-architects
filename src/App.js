a1563d81a1ae4a7cdcce66af67be2059cfa1492c
Alejandro <arodacademic@gmail.com>
2022-01-02 11:20:54 -0500

    feat: Add names routing to switch between portfolios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 
 export default function App() {
   return (
     <>
-      <Home name="alessandro" />
+      <Router>
+        <Switch>
+          <Route path="/alessandro">
+            <Home name="alessandro" />
+          </Route>
+          <Route path="/andrea">
+            <Home name="andrea" />
+          </Route>
+          <Route path="/massimo">
+            <Home name="massimo" />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
